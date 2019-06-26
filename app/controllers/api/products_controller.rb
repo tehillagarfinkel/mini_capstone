86a7c11941e1d34eb9f5ba926679107ed4e7e8e4
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-06-25 19:45:02 -0500

    crate one_product_url to show one product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def one_product_method
+    @products = Product.first
+    render "one_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/all_products_url" => "products#all_products_method"
+    get "/one_product_url" => "products#one_product_method"
   end
 end

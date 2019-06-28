63d0f05289ab076ce5c14f02d88583fa9f6e86d7
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-06-27 20:49:28 -0500

    update methods with update and destroy

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,13 +1,7 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.order(:id)
     render "index.json.jb"
-  end
-
-  def show
-    product_id = params["id"].to_i
-    @product = Product.find_by(id: product_id)
-    render "show.json.jb"
   end
 
   def create
@@ -17,5 +11,28 @@ class Api::ProductsController < ApplicationController
       image_url: params["image_url"],
       description: params["directions"],
     )
+    @product.save
+    render "show.json.jb"
+  end
+
+  def show
+    @product = Product.find_by(id: params["id"])
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params["id"])
+    @product.destroy
+    render json: { message: "Product has been deleted" }
   end
 end

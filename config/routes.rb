73dd72a29974c79da855821ac1ead49c3b7639e3
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-07-07 11:29:41 -0500

    add login with jwt and backend user authentification by creating a user called current user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
 
     get "/products" => "products#index"
     post "/products" => "products#create"

d277eedc30a83cb6a0a0fcdbf431120cfa9b7a9a
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-07-16 19:27:13 -0500

    Add edit, update, and destroy actions for the products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,12 +5,13 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @suppliers = Supplier.all
     render "new.html.erb"
   end
 
   def create
     @product = Product.new(
-      supplier_id: 1,
+      supplier_id: params[:supplier_id],
       name: params[:name],
       price: params[:price],
       description: params[:description],
@@ -22,5 +23,25 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
     render "show.html.erb"
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to "/products"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "/products/new" => "products#new"
   post "/products" => "products#create"
   get "/products/:id" => "products#show"
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 
   namespace :api do
     get "/carted_products" => "carted_products#index"
